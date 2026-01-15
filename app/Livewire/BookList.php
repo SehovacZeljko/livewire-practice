64b08777a3d88e2cf96de12a11feccd6444bc3ac
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2026-01-15 14:21:02 +0100

    Set title with php attributes

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -4,6 +4,9 @@ namespace App\Livewire;
 
 use App\Models\Book;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+
+
 
 class BookList extends Component
 {
@@ -25,7 +28,7 @@ class BookList extends Component
     // {
     //     $this->counter -= 1;
     // }
-
+    #[Title('Book List -Home')]
     public function render()
     {
         return view('livewire.book-list', [
